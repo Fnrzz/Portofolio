a4a852d518906d7f7c8d79c5c6f6e42ff6c1ab6b
Fnrzz <faridnurrganteng123@gmail.com>
2025-05-03 14:24:21 +0700

    [PUSH] change avatar in about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -64,11 +64,11 @@ const About = () => {
           <div className="card-animate bg-[#0E0E10] border-2 border-[#1a1a1a] rounded-xl p-10 md:col-span-1 flex flex-col justify-between">
             <div>
               <Image
-                src="/images/profile.jpg"
+                src="/images/avatar4.png"
                 alt="Avatar"
                 width={150}
                 height={150}
-                className="rounded-[20px] mx-auto mb-10"
+                className=" mx-auto mb-10"
               />
               <h2 className="text-xl font-semibold mb-2">Hi, I'm Farid</h2>
               <p className="text-sm text-gray-300">
@@ -118,7 +118,7 @@ const About = () => {
           </div>
           <div className="card-animate bg-[#0E0E10] border-2 border-[#1a1a1a] rounded-xl p-6 md:col-span-1 md:col-span-2 flex flex-col md:flex-row items-center gap-3">
             <Image
-              src={"/images/developer.png"}
+              src={"/images/avatar5.png"}
               alt={"developer"}
               width={200}
               height={200}
@@ -138,7 +138,7 @@ const About = () => {
         <div className="flex flex-col gap-6">
           <div className="card-animate bg-[#0E0E10] border-2 border-[#1a1a1a] rounded-xl p-6 flex flex-col ">
             <Image
-              src={"/images/freelancer.webp"}
+              src={"/images/avatar1.png"}
               alt="freelancer"
               width={300}
               height={300}
